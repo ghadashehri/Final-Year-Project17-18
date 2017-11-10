4f6a016b709e6ee0a15e021941a23f3c9c0a672b
Ghada alshehri <ghadah.alshehri.2014@cs.rhul.ac.uk>
2017-11-10 22:58:17 +0000

    read each sample .json file and print its content
    still needs a lot of work.
    * Sort or Order 
    * Find common list attributes ?
    * ...

---
+++ Read_Json.py
@@ -0,0 +1,20 @@
+import json
+from pprint import pprint
+
+# open the file and save its contents in data object
+with open('try.json') as data_file:
+    data = json.load(data_file) # take an actual object as parameter
+
+pprint(data)
+
+
+# Sort the files 
+
+
+
+
+# Compare and try to find similarities 
+
+
+
+
