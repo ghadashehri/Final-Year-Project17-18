eea19b1a6cdc6e9f8d86507899dfc800036d1c47
Ghada alshehri <ghadah.alshehri.2014@cs.rhul.ac.uk>
2017-11-17 00:52:50 +0000

    Added more functionality
    it goes through all files in a given Directory, search for .json files, open them, and save method_name. 
    Complexity is now O(n^3)!!!
    need some sort of 2-D to better display data

--- directory_json.py
+++ directory_json.py
@@ -4,23 +4,26 @@ from pathlib import Path
 
 # list of methods called
 Binder_methods = []
-
+files_in_dir = []
 directory_in_str= 'Samples/'
-pathlist = Path(directory_in_str).glob('**/*.json')
+
+#https://stackoverflow.com/questions/10377998/how-can-i-iterate-over-files-in-a-given-directory
+
+
+pathlist= Path(directory_in_str).glob('**/*.json')
 for path in pathlist:
-    # because path is object not string
-    path_in_str = str(path)
-    # print(path_in_str)
+    files_in_dir.append(str(path))
 
-with open(path_in_str) as data_file:
-    # takes an actual object as parameter
-    data = json.load(data_file)
+for f in range(0,len(files_in_dir)):
+    with open(files_in_dir[f]) as data_file:
+        # takes an actual object as parameter
+        data = json.load(data_file)
 
-# Number of elemnts in host
-n = len(data["behaviors"]["dynamic"]["host"])
-for i in range(0,n):
-    m = len(data["behaviors"]["dynamic"]["host"][i]["low"])
-    for j in range(0,m):
-        if(data["behaviors"]["dynamic"]["host"][i]["low"][j]["type"] == 'BINDER'):
-            Binder_methods.append( data["behaviors"]["dynamic"]["host"][i]["low"][j]["method_name"])
+    # Number of elemnts in host
+    n = len(data["behaviors"]["dynamic"]["host"])
+    for i in range(0,n):
+        m = len(data["behaviors"]["dynamic"]["host"][i]["low"])
+        for j in range(0,m):
+            if(data["behaviors"]["dynamic"]["host"][i]["low"][j]["type"] == 'BINDER'):
+                Binder_methods.append( data["behaviors"]["dynamic"]["host"][i]["low"][j]["method_name"])
 print(Binder_methods)
