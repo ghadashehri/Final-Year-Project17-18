fc7012f715ca6c81bb90369f574248c19fc9c894
Ghada alshehri <ghadah.alshehri.2014@cs.rhul.ac.uk>
2017-11-25 17:59:20 +0000

    extracting important information from json files, including classs, subclass and method name

--- Read_Json.py
+++ Read_Json.py
@@ -1,20 +1,49 @@
 import json
+import numpy as np
 from pprint import pprint
+from pathlib import Path
+
 
 # list of methods called
-binder_methods = []
+Binder_methods = []
+class_ = []
+subClass = []
+files_in_dir = []
 
-with open('try.json') as data_file:
+directory_in_str= 'Samples/'
+
+
+
+pathlist= Path(directory_in_str).glob('**/*.json')
+for path in pathlist:
+    files_in_dir.append(str(path))
+
+#for f in range(0,len(files_in_dir)):
+
+with open(files_in_dir[0]) as data_file:
     # takes an actual object as parameter
     data = json.load(data_file)
-
-# Number of elemnts in host
-n = len(data["behaviors"]["dynamic"]["host"])
-for i in range(0,n):
-    m = len(data["behaviors"]["dynamic"]["host"][i]["low"])
-    for j in range(0,m):
-        if(data["behaviors"]["dynamic"]["host"][i]["low"][j]["type"] == 'BINDER'):
-            binder_methods.append( data["behaviors"]["dynamic"]["host"][i]["low"][j]["method_name"])
-print(binder_methods)
+    print data
     
+    # using some of data
+    json_data= data["behaviors"]["dynamic"]["host"]
+    
+n = len(json_data)
+
+for i in range(0,n):
+    m = len(json_data[i]["low"])
+
+    for j in range(0,m):
+        if(json_data[i]["low"][j]["type"] == 'BINDER'):
+            Binder_methods.append(json_data[i]["low"][j]["method_name"])
+            class_.append(json_data[i]["class"])
+            subClass.append(json_data[i]["subclass"])
+
+print(Binder_methods)
+
+print(class_)
+
+print(subClass)
+
+
 
