6d9cfe84fe3943f644e99caa0187b3c791e25c00
Ghada alshehri <ghadah.alshehri.2014@cs.rhul.ac.uk>
2017-11-27 13:05:45 +0000

    Added the System calls and Intent calls to result

--- Read_Json.py
+++ Read_Json.py
@@ -1,6 +1,6 @@
 import json
 import numpy as np
-#import collections
+import collections
 from pprint import pprint
 from pathlib import Path
 from sklearn.feature_extraction import DictVectorizer
@@ -8,51 +8,55 @@ from sklearn.feature_extraction import DictVectorizer
 
 
 vec = DictVectorizer()
-# list of methods called
+
+# Initialising Variabels
+Binder_methods = []
+class_ = []
+subClass = []
 files_in_dir = []
+system_calls= []
 directory_in_str= 'Samples/'
 
-def iterateThroughDir(directory):
-    files= []
-    pathlist= Path(directory).glob('**/*.json')
-    for path in pathlist:
-        files.append(str(path))
-    return files
+# Adding files in directory to a list
+pathlist= Path(directory_in_str).glob('**/*.json')
+for path in pathlist:
+    files_in_dir.append(str(path))
 
-def readJson(f):
-    with open(f) as data_file:
+# Looping through Samples of each malware family
+for f in range(0,len(files_in_dir)):
+    with open(files_in_dir[f]) as data_file:
         # takes an actual object as parameter
         data = json.load(data_file)
 
+
     # using some of data
     json_data= data["behaviors"]["dynamic"]["host"]
-    return json_data
-
-def extractFeature(json_data):
-    Binder_methods = []
-    class_ = []
-    subClass = []
-
     n = len(json_data)
+
     for i in range(0,n):
-        m = len(json_data[i]["low"])
+        # retriving the Binder Calls
+        if(json_data[i]["low"][0]["type"] == 'BINDER'):
+            Binder_methods.append(json_data[i]["low"][0]["method_name"])
+            class_.append(json_data[i]["class"])
+            subClass.append(json_data[i]["subclass"])
+        # retriving Intent Calls
+        elif ( json_data[i]["low"][0]["type"] == 'INTENT' ):
+             Binder_methods.append(json_data[i]["low"][0]["intent"])
+        # retriving System Calls
+        elif(json_data[i]["low"][0]["type"] == 'SYSCALL'):
+            for j in range(0,len(json_data[i]["low"])):
+                system_calls.append(json_data[i]["low"][j]['sysname'])
 
-        for j in range(0,m):
-            if(json_data[i]["low"][j]["type"] == 'BINDER'):
-                Binder_methods.append(json_data[i]["low"][j]["method_name"])
-                class_.append(json_data[i]["class"])
-                subClass.append(json_data[i]["subclass"])
-
-    arr = np.array(Binder_methods)
-    unique, counts = np.unique(arr, return_counts=True)
-    return dict(zip(unique, counts))
-
-
-i=0
-files_in_dir= iterateThroughDir(directory_in_str)
-for f in range(0,len(files_in_dir)):
-    json_data= readJson(files_in_dir[f])
-    data[i] = np.array(extractFeature(json_data))
-    i+=1
-print data
+# Adding both lists together
+joined_list= Binder_methods + system_calls
+arr = np.array(joined_list)
+# Counting the number of times each method/syscall is repeated
+unique, counts = np.unique(arr, return_counts=True)
+print dict(zip(unique, counts))
     
+# additional Counter   
+cnt = collections.Counter()
+for word in arr:
+    cnt[word] += 1
+print cnt.values()
+
