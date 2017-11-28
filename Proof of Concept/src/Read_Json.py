dc5f8a728d53256b2ed09f95574eaa722bcf6642
Ghada alshehri <ghadah.alshehri.2014@cs.rhul.ac.uk>
2017-11-28 00:32:59 +0000

    Updated the structure of class, defined functions to perform certain tasks

--- Proof of Concept/src/Read_Json.py
+++ Proof of Concept/src/Read_Json.py
@@ -4,36 +4,34 @@ import collections
 from pprint import pprint
 from pathlib import Path
 
-# Initialising Variabels
-Binder_methods = []
-class_ = []
-subClass = []
+
+# list of methods called
 files_in_dir = []
-system_calls= []
 directory_in_str= 'Samples/'
+Binder_methods = []
+system_calls= []
 
-# Adding files in directory to a list
-pathlist= Path(directory_in_str).glob('**/*.json')
-for path in pathlist:
-    files_in_dir.append(str(path))
+def iterateThroughDir(directory):
+    files= []
+    pathlist= Path(directory).glob('**/*.json')
+    for path in pathlist:
+        files.append(str(path))
+    return files
 
-# Looping through Samples of each malware family
-for f in range(0,len(files_in_dir)):
-    with open(files_in_dir[f]) as data_file:
+def readJson(f):
+    with open(f) as data_file:
         # takes an actual object as parameter
         data = json.load(data_file)
 
-
     # using some of data
     json_data= data["behaviors"]["dynamic"]["host"]
-    n = len(json_data)
+    return json_data
 
+def extractFeature(json_data):
+    n = len(json_data)
     for i in range(0,n):
-        # retriving the Binder Calls
         if(json_data[i]["low"][0]["type"] == 'BINDER'):
             Binder_methods.append(json_data[i]["low"][0]["method_name"])
-            class_.append(json_data[i]["class"])
-            subClass.append(json_data[i]["subclass"])
         # retriving Intent Calls
         elif ( json_data[i]["low"][0]["type"] == 'INTENT' ):
              Binder_methods.append(json_data[i]["low"][0]["intent"])
@@ -42,12 +40,16 @@ for f in range(0,len(files_in_dir)):
             for j in range(0,len(json_data[i]["low"])):
                 system_calls.append(json_data[i]["low"][j]['sysname'])
 
-# Adding both lists together
-joined_list= Binder_methods + system_calls
-arr = np.array(joined_list)
-# Counting the number of times each method/syscall is repeated
-unique, counts = np.unique(arr, return_counts=True)
 
+
+files_in_dir= iterateThroughDir(directory_in_str)
+for f in range(0,len(files_in_dir)):
+    json_data= readJson(files_in_dir[f])
+    extractFeature(json_data)
+
+combined = Binder_methods + system_calls
+arr = np.array(combined)
+unique, counts = np.unique(arr, return_counts=True)
 print"\nThe methods Calls in all submited Samples: "
 print dict(zip(unique, counts))
 print "\n"
