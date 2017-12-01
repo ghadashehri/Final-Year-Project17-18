f8e5b54edee830adbbc70a04dfc3b8d6bad699a2
Ghada alshehri <ghadah.alshehri.2014@cs.rhul.ac.uk>
2017-12-01 20:11:32 +0000

    Update Test_ParseJson.py

--- Proof of Concept/test/Test_ParseJson.py
+++ Proof of Concept/test/Test_ParseJson.py
@@ -2,20 +2,25 @@
 Created on 28 Nov 2017
 
 @author: Ghadah.
+
+A test case to check the results obtained from ParseJson class are correct
+it checks all methods found in that class.
 '''
 import unittest
 import json
 from pathlib import Path
-from ParseJson import iterateThroughDir,readJson,extractFeature
+from ParseJson import iterateThroughDir, readJson, extractFeature
 
 Binder_methods = []
 system_calls = []
-class Test_Read_Json(unittest.TestCase):
+
+
+class Test_ParseJson(unittest.TestCase):
 
     def test_iterateThroughDir(self):
-        files =[] 
-        directory = 'samples/'
-        pathlist= Path(directory).glob('**/*.json')
+        files = []
+        directory = '../../samples/'
+        pathlist = Path(directory).glob('**/*.json')
         for path in pathlist:
             files.append(str(path))
         method_files = iterateThroughDir(directory)
@@ -26,17 +31,16 @@ class Test_Read_Json(unittest.TestCase):
         f = 'try.json'
         with open(f) as data_file:
             data = json.load(data_file)
-            method_data =  readJson(f)
+            method_data = readJson(f)
 
         data = data["behaviors"]["dynamic"]["host"]
         # Check thats the correct file is read
-        self.assertEqual(method_data,data)
+        self.assertEqual(method_data, data)
 
-        # Check that readJson fails when the passed variable is file, not string.
+        # Check that readJson fails when the passed variable is file not string
         with self.assertRaises(TypeError):
             readJson(data_file)
-            
-            
+
     def test_extractFeature(self):
         methods = []
         f = 'try.json'
@@ -44,8 +48,8 @@ class Test_Read_Json(unittest.TestCase):
             data = json.load(data_file)
         data = data["behaviors"]["dynamic"]["host"]
         n = len(data)
-        for i in range(0,n):
-            if ( data[i]["low"][0]["type"] == 'INTENT' ):
+        for i in range(0, n):
+            if (data[i]["low"][0]["type"] == 'INTENT'):
                 Binder_methods.append(data[i]["low"][0]["intent"])
         extractFeature(data)
         self.assertEqual(Binder_methods, methods)
