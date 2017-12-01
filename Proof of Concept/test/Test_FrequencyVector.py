6e6dce98c93908fa7f2f27bfc1565567d11c12f6
Ghada alshehri <ghadah.alshehri.2014@cs.rhul.ac.uk>
2017-12-01 20:09:34 +0000

    Update Test_FrequencyVector.py

--- Proof of Concept/test/Test_FrequencyVector.py
+++ Proof of Concept/test/Test_FrequencyVector.py
@@ -1,4 +1,3 @@
-
 '''
 Created on 28 Nov 2017
 
@@ -7,16 +6,18 @@ Created on 28 Nov 2017
 import unittest
 import json
 from pathlib import Path
-from BitVector import iterateThroughDir,readJson,extractFeature
+from FrequencyVector import iterateThroughDir, readJson, extractFeature
 
 Binder_methods = []
 system_calls = []
-class Test_Read_Json(unittest.TestCase):
+
+
+class Test_FrequencyVector(unittest.TestCase):
 
     def test_iterateThroughDir(self):
-        files =[] 
-        directory = 'samples/'
-        pathlist= Path(directory).glob('**/*.json')
+        files = []
+        directory = '../../samples/'
+        pathlist = Path(directory).glob('**/*.json')
         for path in pathlist:
             files.append(str(path))
         method_files = iterateThroughDir(directory)
@@ -27,17 +28,16 @@ class Test_Read_Json(unittest.TestCase):
         f = 'try.json'
         with open(f) as data_file:
             data = json.load(data_file)
-            method_data =  readJson(f)
+            method_data = readJson(f)
 
         data = data["behaviors"]["dynamic"]["host"]
         # Check thats the correct file is read
-        self.assertEqual(method_data,data)
+        self.assertEqual(method_data, data)
 
-        # Check that readJson fails when the passed variable is file, not string.
+        # Check that readJson fails when the passed variable is file not string
         with self.assertRaises(TypeError):
             readJson(data_file)
-            
-            
+
     def test_extractFeature(self):
         methods = []
         f = 'try.json'
@@ -45,12 +45,12 @@ class Test_Read_Json(unittest.TestCase):
             data = json.load(data_file)
         data = data["behaviors"]["dynamic"]["host"]
         n = len(data)
-        for i in range(0,n):
-            if ( data[i]["low"][0]["type"] == 'INTENT' ):
+        for i in range(0, n):
+            if (data[i]["low"][0]["type"] == 'INTENT'):
                 Binder_methods.append(data[i]["low"][0]["intent"])
         extractFeature(data)
         self.assertEqual(Binder_methods, methods)
 
+
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
