42e44fa3ecc45071dc0ffb1567e2d5409ba60673
Ghada alshehri <ghadah.alshehri.2014@cs.rhul.ac.uk>
2018-02-01 00:41:05 +0000

    Improved the plot functions.
    Got a higher accuracy this time 66.82, with frequency vectors and Eps= 3.9 and MinPts= 10

--- DBSCAN/DbscanAlgo.py
+++ DBSCAN/DbscanAlgo.py
@@ -3,17 +3,17 @@ Created on 1 Dec 2017
 
 @author: Ghadah
 
-    #### DOESNT WORK JUST TO EXPERIMENT  WITH ####
  A class to experiment with existing implementation of DBSCAN implementation
  taken from http://scikit-learn.org/stable/auto_examples/cluster/plot_dbscan.html#sphx-glr-auto-examples-cluster-plot-dbscan-py
 '''
 
 from sklearn.cluster import DBSCAN
-from sklearn import metrics
+#from sklearn import metrics
 from sklearn.preprocessing import StandardScaler
-# from FrequencyVector import getFrequencyVector
+from FrequencyVector import getFrequencyVector
 from matplotlib import pyplot as plt
 from BitVector import getBitVector
+#from notInc import Bitv
 import numpy as np
 
 # Initialise Variables
@@ -21,8 +21,10 @@ features_w_labels = []
 features = []
 
 # Get extracted features from Bitvector class
-data = getBitVector()
-# data = getFrequencyVector()
+#data = Bitv.getBitV()
+#data = getBitVector()
+data = getFrequencyVector()
+
 
 # Extract vectors in a list
 i = 0
@@ -38,7 +40,6 @@ for key in data:
         features_w_labels.append(val)
     i = i + 1
 
-# print(features)
 
 # Represent data as a matrix
 mat = np.matrix(features)
@@ -50,63 +51,93 @@ for i in range(0, 1):
 # Normalise Data
 stscaler = StandardScaler().fit(mat)
 matrix = stscaler.transform(mat)
-# print(mat)
 
-for i in range(0, 3):
+for i in range(0, 30):
     print(matrix[i])
 
 
 # Compute DBSCAN
-model = DBSCAN(eps=1.5, min_samples=8).fit(matrix)
-model2 = DBSCAN(eps=1, min_samples=12).fit_predict(matrix, y=None)
-# print(model2)
-labels = model.labels_
+dbscn = DBSCAN(eps=3.8, min_samples=25).fit(matrix)
+#model2 = DBSCAN(eps=1, min_samples=12).fit_predict(matrix, y=None)
+labels = dbscn.labels_
+core_samples = np.zeros_like(labels, dtype=bool)
+core_samples[dbscn.core_sample_indices_] = True
+
+
 print(labels)
 print('\n\n')
+
 for i in range(0, 3):
     print(labels[i])
 
 
-# Number of clusters in labels, ignoring noise if present.
-n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-# unique_labels = set(model.lab)
-core = np.zeros_like(model.labels_, dtype=bool)
-core[model.core_sample_indices_] = True
-
-unique_labels = set(labels)
-colors = [plt.cm.Spectral(each)
-          for each in np.linspace(0, 1, len(unique_labels))]
-for k, col in zip(unique_labels, colors):
-    if k == -1:
-        # Black used for noise.
-        col = [0, 0, 0, 1]
-
-    class_member_mask = (labels == k)
-
-    #xy = matrix[class_member_mask & core]
-    plt.plot(matrix[:, 0], matrix[:, 1], 'o', markerfacecolor=tuple(col),
-             markeredgecolor='k', markersize=14)
-
-    xy = matrix[class_member_mask & ~core]
-    plt.plot(matrix[:, 0], matrix[:, 1], 'o', markerfacecolor=tuple(col),
-             markeredgecolor='k', markersize=6)
-
-plt.title('Estimated number of clusters: %d' % n_clusters_)
-#plt.show()
-
-
 # Check Accuracy of Algorithm
-accuracy = 0
-matrix_w_labels = np.matrix(features_w_labels)
-for i in range(0, len(labels)):
-    if matrix_w_labels[i].item(18) == labels[i]:
-        accuracy = accuracy + 1
-#     else:
-#         accuracy = accuracy - 1
-
-print("DBSCAN ALGORITHM ACCURACY ", (accuracy/208)*100)
+def get_accuracy(labels):
+    acc = 0
+    matrix_w_labels = np.matrix(features_w_labels)
+    for i in range(0, len(labels)):
+        if matrix_w_labels[i].item(18) == labels[i]:
+            acc = acc + 1
+    return("DBSCAN ALGORITHM ACCURACY ", (acc/208)*100)
 
 
+accuracy = get_accuracy(labels)
+print(accuracy)
+
+
+# PLOTTTINNNNGGGG DDDDAAATTTTAAAAA #####+++++++++++++++++###############
+
+# Display sample data
+for i in range(0, matrix.shape[0]):
+    plt.plot(matrix[i, :])
+plt.xlabel("X-Axis")
+plt.ylabel("Y-Axis")
+plt.title("Android Sample Data ")
+plt.show()
+
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+unique_labels = np.unique(labels)
+colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
+
+for (label, color) in zip(unique_labels, colors):
+    class_member_mask = (labels == label)
+    xy = matrix[class_member_mask & core_samples]
+    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=color, markersize=14)
+
+    xy2 = matrix[class_member_mask & ~core_samples]
+    plt.plot(xy2[:, 0], xy2[:, 1], 'o', markerfacecolor=color, markersize=6)
+plt.title("DBSCAN on Clustered data")
+plt.title('Estimated number of clusters: %d' % n_clusters_)
+plt.xlabel("X (scaled)")
+plt.ylabel("Y (scaled)")
+plt.show()
+
+
+# # Number of clusters in labels, ignoring noise if present.
+# n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+# # unique_labels = set(model.lab)
+# 
+# 
+# unique_labels = set(labels)
+# colors = [plt.cm.Spectral(each)
+#           for each in np.linspace(0, 1, len(unique_labels))]
+# for k, col in zip(unique_labels, colors):
+#     if k == -1:
+#         # Black used for noise.
+#         col = [0, 0, 0, 1]
+# 
+#     class_member_mask = (labels == k)
+# 
+#     #xy = matrix[class_member_mask & core]
+#     plt.plot(matrix[:, 0], matrix[:, 1], 'o', markerfacecolor=tuple(col),
+#              markeredgecolor='k', markersize=14)
+# 
+#     xy = matrix[class_member_mask & ~core]
+#     plt.plot(matrix[:, 0], matrix[:, 1], 'o', markerfacecolor=tuple(col),
+#              markeredgecolor='k', markersize=6)
+# 
+# plt.title('Estimated number of clusters: %d' % n_clusters_)
+# #plt.show()
 
 
 
